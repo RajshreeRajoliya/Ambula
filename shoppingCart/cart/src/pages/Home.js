6c174399eefd504407841bafb61847eb76a969da
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-07-15 18:36:35 +0530

    final

--- shoppingCart/cart/src/pages/Home.js
+++ shoppingCart/cart/src/pages/Home.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import productsData from '../data/productsData';
+import React, { useEffect, useState } from 'react';
+
 import ProductsCard from '../components/ProductsCard';
+import axios from 'axios'
 
 const Home = () => {
+    const URL = "https://backendambula.onrender.com/aproducts"
+    const[data, setData] = useState([]);
+
+    const getData = async() =>{
+    const response = await axios.get(URL);
+    console.log("data",response.data);
+    setData(response.data)
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
     return (
         <>
             <section id="home">
                 <div className="container">
                     <div className="home_content">
                         {
-                            productsData.map((item) => (
+                            data.map((item) => (
                                 <ProductsCard key={item.id} {...item} />
                             ))
                         }
